770d36218a29e10ffea8293ae2c8a0e7f8123f7d
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2025-03-14 21:58:09 +0500

    Add JWT utility functions for signing and verifying tokens

---
+++ src/tools/jwt.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken"
+const SECRET_KEY = process.env("JWT_SECRET_KEY")
+
+
+const sign = (payload) => {
+    return jwt.sign(payload, SECRET_KEY, { expiresIn: "1d" })
+}
+
+const verify = (token) => {
+    return jwt.verify(token, SECRET_KEY)
+}
+
+export { sign, verify }
