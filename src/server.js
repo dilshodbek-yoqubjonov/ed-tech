f019233475524ba3a52d8d9a5b7b0244ec91da98
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2025-03-15 15:07:11 +0500

    Refactor server.js to include additional routers and cookie-parser middleware

--- src/server.js
+++ src/server.js
@@ -1,16 +1,34 @@
 import express from "express";
-import { courseRouter } from "./routes/coursesRouter.js";
+import cookieParser from "cookie-parser";
 
+// imported routes
+import { courseRouter } from "./routes/course.router.js";
+import { adminRouter } from "./routes/admin.router.js";
+import { enrolmentRouter } from "./routes/enrollment.router.js";
+import { userRouter } from "./routes/user.router.js";
+import { videoRouter } from "./routes/video.router.js";
+import { analyseRouter } from "./routes/analytics.router.js";
+import { teacherRouter } from "./routes/teacher.router.js";
+import { AiRouter } from "./routes/ai.router.js";
 
+// server
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// middlewares
 app.use(express.json({ limit: "50mb" }));
+app.use(cookieParser())
 
-
+// main routers
 app.use("/courses", courseRouter);
+app.use(adminRouter)
+app.use("/enrolment", enrolmentRouter)
+app.use("/user", userRouter)
+app.use("/video", videoRouter)
+app.use("/", analyseRouter)
+app.use("/teacher", teacherRouter)
+app.use("/ai", AiRouter)
 
 
-console.clear()
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
